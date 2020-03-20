d057fc11d14feda76cc7c0dee1b3d529c4794ce1
SzAttila97 <39098456+SzAttila97@users.noreply.github.com>
2020-03-20 11:45:23 +0100

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCodingConsole
+{
+    public class Node
+    {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            // Leaf
+            if (Right == null && Left == null)
+            {
+                if (symbol.Equals(this.Symbol))
+                {
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+
+                    left = Left.Traverse(symbol, leftPath);
+                    for (int i = 0; i < leftPath.Count; i++)
+                    {
+                        Console.WriteLine("LeftPath :" + leftPath[i]);
+                    }
+                }
+
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(symbol, rightPath);
+
+                    for (int i = 0; i < rightPath.Count; i++)
+                    {
+                        Console.WriteLine("RightPath :" + rightPath[i]);
+                    }
+                }
+
+                if (left != null)
+                {
+                    return left;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
